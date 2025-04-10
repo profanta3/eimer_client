d31a38040c72a53178b6f4224b624b9bb1e3d508
profanta <dev@profanta.de>
2025-04-10 20:58:07 +0200

    refactor: code structure

---
+++ eimer_client/__init__.py
@@ -0,0 +1,11 @@
+from .base import BaseClient
+from .move import Move
+from .config import ClientConfig
+from .log import log
+
+__all__ = [
+    "BaseClient",
+    "Move",
+    "ClientConfig",
+    "log",
+]
